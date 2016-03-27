2c32a69d47c5ebd57575b173be041ffdd15244b5
bShaak <bradenshaak@gmail.com>
2016-03-27 14:49:34 -0700

    refactored into a class

--- hockey_ipsum.py
+++ hockey_ipsum.py
@@ -1,22 +1,31 @@
 import os
 
-files = []
+class HockeyIpsum:
+    def __init__(self, directory = "/wordbank"):
+        self.files = []
+        self.words = []
+        self.wordbank_dir = "." + directory + "/"
+        self._load_files()
+        self._read_files()
 
-for f in os.listdir("./wordbank"):
-    if f.endswith(".txt"):
-        print "loading " + f
-        files.append("./wordbank/"+f)
+    def _load_files(self):
+        for f in os.listdir(self.wordbank_dir):
+            if f.endswith(".txt"):
+                print "loading " + f
+                self.files.append(self.wordbank_dir + f)
 
-words = []
+    def _read_files(self):
+        i = 0;
+        for filename in self.files:
+            f = open(filename, 'r')
+            for line in f:
+                self.words.append(line.rstrip())
+                i = i+1
+            f.close()
 
-i = 0;
-for filename in files:
-    f = open(filename, 'r')
-    for line in f:
-        words.append(line.rstrip())
-        i = i+1
-    f.close()
+    def print_wordbank(self):
+        for w in self.words:
+            print w
 
-
-for w in words:
-    print w
+hockey = HockeyIpsum()
+hockey.print_wordbank()
