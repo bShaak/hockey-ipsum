f211f65e2accd554a3728510e92c265517502226
bShaak <bradenshaak@gmail.com>
2016-03-27 15:30:37 -0700

    create titles and paragraphs

--- hockey_ipsum.py
+++ hockey_ipsum.py
@@ -1,20 +1,21 @@
 import os
+import random
 
 class HockeyIpsum:
     def __init__(self, directory = "/wordbank"):
         self.files = []
         self.words = []
         self.wordbank_dir = "." + directory + "/"
-        self._load_files()
-        self._read_files()
+        self._loadfiles()
+        self._readfiles()
 
-    def _load_files(self):
+    def _loadfiles(self):
         for f in os.listdir(self.wordbank_dir):
             if f.endswith(".txt"):
                 print "loading " + f
                 self.files.append(self.wordbank_dir + f)
 
-    def _read_files(self):
+    def _readfiles(self):
         i = 0;
         for filename in self.files:
             f = open(filename, 'r')
@@ -23,9 +24,30 @@ class HockeyIpsum:
                 i = i+1
             f.close()
 
-    def print_wordbank(self):
+    def printwordbank(self):
         for w in self.words:
             print w
 
+    #getwords returns a string of length from the wordbank
+    def getwords(self, size):
+        tmp_words = []
+        max_index = len(self.words) - 1
+        #create list of random integers between 0 and self.words.length
+        for i in range(size):
+            tmp_words.append(self.words[random.randrange(0, max_index, 1)])
+        return ' '.join(tmp_words)
+
+    #gettitle returns a 1-5 words string.
+    def gettitle(self):
+        num_words = random.randrange(1,5,1)
+        return self.getwords(num_words)
+
+    #returns a 100-200 word string
+    def getparagraph(self):
+        num_words = random.randrange(100,200,1)
+        return self.getwords(num_words)
+
+
 hockey = HockeyIpsum()
-hockey.print_wordbank()
+print hockey.gettitle()
+#print hockey.getparagraph()
